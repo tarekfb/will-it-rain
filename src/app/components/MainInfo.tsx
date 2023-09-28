52ac31fab67316c500cb1462f0b6a4dfc2c80f04
tabeGH <tarek.bermalm@knightec.se>
2023-09-28 12:36:18 +0200

    implemented dynamic rendering with nextjs

--- src/app/components/MainInfo.tsx
+++ src/app/components/MainInfo.tsx
@@ -1,60 +1,34 @@
 "use client";
-import { ReactElement, useEffect, useState } from "react";
-import {
-  BsDroplet,
-  BsDropletHalf,
-  BsDropletFill,
-  BsCloudRainHeavyFill,
-} from "react-icons/bs";
-import { TbDropletOff, TbQuestionMark, TbDropletHalf2 } from "react-icons/tb";
+import { ReactElement } from "react";
 
-export default function MainInfo() {
-  const [percentage, setPercentage] = useState<number>(NaN);
-  const [word, setWord] = useState<string>("");
-  const [icon, setIcon] = useState<ReactElement | null>(null);
+type Props = {
+  perc: number;
+  icon: ReactElement;
+  word: string;
+};
 
-  const calcWord = (perc: number) => {
-    if (perc < 5) return "No";
-    if (perc < 25) return "Probably not";
-    if (perc < 50) return "Perhaps";
-    if (perc < 75) return "Probably";
-    if (perc < 95) return "Most likely";
-    if (perc < 100) return "Yes";
-    return "Unsure";
-  };
-
-  const calcIcon = (perc: number): ReactElement | null => {
-    const size = 140;
-    if (perc < 5) return <TbDropletOff size={size} />;
-    if (perc < 25) return <BsDroplet size={size} />;
-    if (perc < 50) return <BsDropletHalf size={size} />;
-    if (perc < 75) return <TbDropletHalf2 size={size} />;
-    if (perc < 95) return <BsDropletFill size={size} />;
-    if (perc < 100 || perc === 100) return <BsCloudRainHeavyFill size={size} />;
-    return <TbQuestionMark size={size} />;
-  };
-
-  useEffect(() => {
-    const getProbability = async () => {
-      const api =
-        "https://api.open-meteo.com/v1/forecast?latitude=59.33&longitude=18.07&daily=precipitation_probability_max&timezone=Europe%2FBerlin";
-      const data = await fetch(api);
-      const obj = await data.json();
-      const percentage = obj.daily.precipitation_probability_max[0];
-      const isBetweenZeroAndHundred = /^([1-9]\d|\d|100)$/.test(percentage);
-      if (isBetweenZeroAndHundred) {
-        const word = calcWord(percentage);
-        const icon = calcIcon(percentage);
-        setPercentage(percentage);
-        setWord(word);
-        setIcon(icon);
-      } else {
-        setWord("Something went wrong");
-        setIcon(<TbQuestionMark size={140} />);
-      }
-    };
-    getProbability();
-  }, []);
+export default function MainInfo({ perc, icon, word }: Props) {
+  // useEffect(() => {
+  //   const getProbability = async () => {
+  //     const api =
+  //       "https://api.open-meteo.com/v1/forecast?latitude=59.33&longitude=18.07&daily=precipitation_probability_max&timezone=Europe%2FBerlin";
+  //     const data = await fetch(api);
+  //     const obj = await data.json();
+  //     const percentage = obj.daily.precipitation_probability_max[0];
+  //     const isBetweenZeroAndHundred = /^([1-9]\d|\d|100)$/.test(percentage);
+  //     if (isBetweenZeroAndHundred) {
+  //       const word = calcWord(percentage);
+  //       const icon = calcIcon(percentage);
+  //       setPercentage(percentage);
+  //       setWord(word);
+  //       setIcon(icon);
+  //     } else {
+  //       setWord("Something went wrong");
+  //       setIcon(<TbQuestionMark size={140} />);
+  //     }
+  //   };
+  //   getProbability();
+  // }, []);
 
   return (
     <>
@@ -62,9 +36,9 @@ export default function MainInfo() {
       {icon}
       <div className="flex flex-col items-center justify-center space-y-2">
         <h2 className="text-3xl font-bold mr-2">{word && word}</h2>
-        {percentage >= 0 && percentage <= 100 && (
+        {perc >= 0 && perc <= 100 && ( // if a number is 0, 100, or in between -> if everything is working
           <h3 className="text-xl">
-            <strong>{`${percentage}%`}</strong> chance of rain
+            <strong>{`${perc}%`}</strong> chance of rain
           </h3>
         )}
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,82 @@
+import { ReactElement } from "react";
 import MainInfo from "./components/MainInfo";
+import {
+  BsDroplet,
+  BsDropletHalf,
+  BsDropletFill,
+  BsCloudRainHeavyFill,
+} from "react-icons/bs";
+import { TbDropletOff, TbQuestionMark, TbDropletHalf2 } from "react-icons/tb";
 
-export default function Home() {
+async function getWeather(): Promise<Weather> {
+  const url =
+    "https://api.open-meteo.com/v1/forecast?latitude=59.33&longitude=18.07&daily=precipitation_probability_max&timezone=Europe%2FBerlin";
+  const res = await fetch(url);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return await res.json();
+}
+
+async function getPercentage(): Promise<number> {
+  const responseString = await getWeather();
+  console.log(responseString);
+  return responseString.daily.precipitation_probability_max[0];
+}
+
+type Weather = {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  daily_units: {
+    time: string;
+    precipitation_probability_max: string;
+  };
+  daily: {
+    time: string[];
+    precipitation_probability_max: number[];
+  };
+};
+
+const calcWord = (perc: number) => {
+  if (perc < 5) return "No";
+  if (perc < 25) return "Probably not";
+  if (perc < 50) return "Perhaps";
+  if (perc < 75) return "Probably";
+  if (perc < 95) return "Most likely";
+  if (perc < 100) return "Yes";
+  return "We're not sure";
+};
+
+const calcIcon = (perc: number): ReactElement => {
+  const size = 140;
+  if (perc < 5) return <TbDropletOff size={size} />;
+  if (perc < 25) return <BsDroplet size={size} />;
+  if (perc < 50) return <BsDropletHalf size={size} />;
+  if (perc < 75) return <TbDropletHalf2 size={size} />;
+  if (perc < 95) return <BsDropletFill size={size} />;
+  if (perc < 100 || perc === 100) return <BsCloudRainHeavyFill size={size} />;
+  return <TbQuestionMark size={size} />;
+};
+
+export default async function Home() {
+  const perc = await getPercentage();
+  const icon = calcIcon(perc);
+  const word = calcWord(perc);
+
   return (
     <div className="min-h-screen min-w-screen flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center space-y-10">
-        <MainInfo />
+        <MainInfo perc={perc} word={word} icon={icon} />
       </div>
     </div>
   );
