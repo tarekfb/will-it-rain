b72c0d570c6717649f2db7dbdd86b0d8801946da
tabeGH <tarek.bermalm@knightec.se>
2023-10-31 16:32:21 +0100

    fix path

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,2 +1,2 @@
 export const basePath = (): string =>
-  `${process.env.NODE_ENV === "development" ? "" : "http://localhost:3000"}/api/`;
+  `${process.env.NODE_ENV === "production" ? "" : "http://localhost:3000"}/api/`;
