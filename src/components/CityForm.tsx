9b85da4e32977ef63c1e0e4b8ea051ad54cdfd04
tabeGH <tarek.bermalm@knightec.se>
2023-11-14 15:57:47 +0100

    add proper input styling

--- src/components/CityForm.tsx
+++ src/components/CityForm.tsx
@@ -2,11 +2,13 @@ import { FormEvent, useId, useState } from "react";
 import { getCity } from "src/utils/api-calls-internal";
 import { getErrorMessage } from "src/utils/utils";
 import { City } from "utils/types";
+import { CiSearch } from "react-icons/ci";
 
 type Props = {
   setCity: (city: City) => void;
 };
 
+const h = 8;
 export default function Form({ setCity }: Props) {
   const cityInputId = useId();
   const [value, setValue] = useState("");
@@ -23,18 +25,29 @@ export default function Form({ setCity }: Props) {
 
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
-      <label>
-        <label htmlFor={cityInputId} className="hidden">City</label>
-        <input
-          id={cityInputId}
-          name="city"
-          placeholder="Search for a city"
-          type="text"
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
-          className="border-zinc-600 rounded-md border-solid p-1"
-        />
-      </label>
+      <div
+        className={`flex space-x-4 justify-center border-zinc-600 rounded-full border-solid bg-white px-8 h-${h}`}
+      >
+        <div className="flex items-center">
+          <CiSearch className="text-gray-600" size={24} />
+        </div>
+        <div>
+          <label>
+            <label htmlFor={cityInputId} className="hidden">
+              City
+            </label>
+            <input
+              id={cityInputId}
+              name="city"
+              placeholder="Search for a city"
+              type="text"
+              onChange={(e) => setValue(e.target.value)}
+              value={value}
+              className={`text-gray-600 outline-none h-${h} leading-${h} w-40`}
+            />
+          </label>
+        </div>
+      </div>
     </form>
   );
 }
